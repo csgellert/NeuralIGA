e0c21cf6a417335651082e0cef0fd3b0efde06d9
csgellert <csapodi.gellert@gmail.com>
2024-03-05 15:18:09 +0100

    fixed FEM solver

--- 1D-IGA-Laplace/FEM.py
+++ 1D-IGA-Laplace/FEM.py
@@ -5,6 +5,7 @@ def element(xx, t, k):
     grad = len(t)-k-1
     K = np.zeros([grad,grad])
     F = np.zeros(grad)
+    dx = (xx[2]-xx[1])
     for i in range(grad):
         if i == 0: continue
         for j in range(grad):
@@ -14,26 +15,34 @@ def element(xx, t, k):
             # dNj = [dBdXi(x,k,j,t) for x in xx]
             #Numerical integration:
             for x in xx:
-                K[i,j] += dBdXi(x,k,i,t)*dBdXi(x,k,j,t)*(xx[1]-xx[2])
+                K[i,j] += dBdXi(x,k,i,t)*dBdXi(x,k,j,t)*dx
 
             #ax.plot(xx2, [dBdXi(x,k,i,t) for x in xx])
         fsdf = xx[-1]
         d = dBdXi(xx[-2],k,i,t)
-        F[i] = 50* d
+
+        for x in xx:
+            F[i] += B(x,k,i,t)*(6-6*x+12*x**2-20*x**3)*dx
+        F[i]+= 50* B(xx[-2],k,i,t)
+    print(K)
+    print(F)
     return K,F
 def assembly():
     pass
 def solve(F,K):
     u = np.dot(np.linalg.inv(K[1:,1:]),F[1:])
-    u = np.insert(u,0,0)
+    print("U:\n",u)
     return u
-def show(u,t,k,xx):
+def show(u,t,k,xx,analitical = None):
     sum = np.zeros(xx.shape)
-    for i in range(len(t)-k-1):
+    for i in range(1,len(t)-k-1):
         Ni = np.array([B(x,k,i,t) for x in xx])
-        ui = u[i]
+        ui = u[i-1]
         Ni = ui*Ni
         plt.plot(xx, Ni,'--g')
         sum +=Ni
+    sum+=1
     plt.plot(xx,sum)
+    if analitical:
+        plt.plot(xx,analitical)
     plt.show()
--- 1D-IGA-Laplace/main.py
+++ 1D-IGA-Laplace/main.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 import FEM
 from scipy.interpolate import BSpline
 print("\n\nSolving one dimensional Laplace equation with IGA.\n","-"*50,"\n")
-print("Laplace equation:\t\td^2 f(x) / dx^2 = 0\n\n")
-
+print("Laplace equation:\t\t-d^2 f(x) / dx^2 = 6 - 6x + 12x^2 -20x^3\n\n")
+print("RB1: f(0) = 1\nf'(2)=50")
 #Endpoints of the domain
 xA = 0
 xB = 2
 l = xB-xA # length of the domain
 
 #Number of elements
-N = 4
+N = 5
 p = 2 #quadratic B-splines
 
 # defining the domain 
@@ -19,16 +19,16 @@ knots = np.linspace(xA, xB, N)
 knots = np.insert(knots,0,[xA,xA])
 knots = np.append(knots,[xB,xB])
 #knots = [0, 0, xB, xB]
-print(knots)
+#print(knots)
 
 c = np.ones(N)
 #print(BSpline(knots,c,p))
-x = np.linspace(xA,xB,100)
+x = np.linspace(xA,xB,1000)
 
 K,F = FEM.element(x,knots,p)
 u = FEM.solve(F,K)
-FEM.show(u,knots,p,x)
-input("f")
+analitical = [xv**5-xv**4+xv**3-3*xv*xv+2*xv+1 for xv in x]
+FEM.show(u,knots,p,x, analitical)
 
 
 
